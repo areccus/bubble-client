ed1b2c99ac48092dea3ed991d4f0b548b6c5b00f
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-26 17:39:04 -0400

    posts should pop up with the latest being at the top

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -36,36 +36,35 @@ const PostsWidget = ({ userId, isProfile = false }) => {
       getPosts();
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
+  const sortedPosts = Array.isArray(posts) ? [...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) : [];
   return (
     <>
-    {Array.isArray(posts) &&
-      posts.map(
-        ({
-          _id,
-          userId,
-          userName,
-          description,
-          location,
-          picturePath,
-          userPicturePath,
-          likes,
-          comments,
-        }) => (
-          <PostWidget
-            key={_id}
-            postId={_id}
-            postUserId={userId}
-            name={`${userName}`}
-            description={description}
-            location={location}
-            picturePath={picturePath}
-            userPicturePath={userPicturePath}
-            likes={likes}
-            comments={comments}
-          />
-        )
-      )}
+    {sortedPosts.map(
+  ({
+    _id,
+    userId,
+    userName,
+    description,
+    location,
+    picturePath,
+    userPicturePath,
+    likes,
+    comments,
+  }) => (
+    <PostWidget
+      key={_id}
+      postId={_id}
+      postUserId={userId}
+      name={`${userName}`}
+      description={description}
+      location={location}
+      picturePath={picturePath}
+      userPicturePath={userPicturePath}
+      likes={likes}
+      comments={comments}
+    />
+  )
+)}
   </>
   );
 };
